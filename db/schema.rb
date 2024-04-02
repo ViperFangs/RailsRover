f93084792a632b58c689909b51a7668d16cea896
ViperFangs <amathrej@asu.edu>
2024-04-02 01:32:10 -0700

    Generate article model with title and body

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_28_200700) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_02_082510) do
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "cars", force: :cascade do |t|
     t.string "make"
     t.string "model"
